71831eafdb3a3d76db0329ac06a65772a0b05fc1
JakubMaka1 <jakubmaka1@gmail.com>
2024-08-22 15:00:33 +0200

    Dodanie parowania json, zmiany statusu policy przez polecenie PUT oraz sprawdzenie GET,

--- Program.cs
+++ Program.cs
@@ -6,27 +6,14 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
+using Org.BouncyCastle.Tls;
+using Newtonsoft.Json.Linq;
 
 class Program
 {
     static async Task Main(string[] args)
     {
-        Stopwatch stopwatch = new Stopwatch();
-        stopwatch.Start();
-
-        Console.WriteLine("Program rozpoczął działanie.");
-
-        await IMAP();
-
-        stopwatch.Stop();
-
-        Console.WriteLine("Program zakończył działanie.");
-        TimeSpan elapsedTime = stopwatch.Elapsed;
-        Console.WriteLine($"Czas działania programu: {elapsedTime.Hours} godzin, {elapsedTime.Minutes} minut, {elapsedTime.Seconds} sekund, {elapsedTime.Milliseconds} milisekund");
-    }
-
-    static async Task IMAP()
-    {
+          Console.WriteLine($"Program rozpoczął działanie. {DateTime.Now}");
         // Pobieranie danych logowania ze zmiennych środowiskowych
         string email = Environment.GetEnvironmentVariable("EMAIL_ADDRESS");
         string password = Environment.GetEnvironmentVariable("EMAIL_PASSWORD");
@@ -55,7 +42,6 @@ class Program
                 var cancellationTokenSource = new CancellationTokenSource();
                 // Pobierz nową wiadomość
                     var newest_message = await inbox.GetMessageAsync(inbox.Count - 1);
-                     Console.WriteLine($"{DateTime.Today} Rozpoczęto program");
                      Console.WriteLine($"Najnowsza wiadomość to: {newest_message.Subject}");
                      Console.WriteLine("");
                      Console.WriteLine("Monitoring folderu INBOX...");
@@ -77,10 +63,30 @@ class Program
                           var message = await inbox.GetMessageAsync(uid);
                           Console.WriteLine($"Odebrano wiadomość: {message.Subject}");
                          
-                          if (message.Subject.Contains("test", StringComparison.OrdinalIgnoreCase))
+                          if (message.Subject.Contains("STOP", StringComparison.OrdinalIgnoreCase))
                             {
+                                string jsonData = "{ \"status\": \"disable\" }";
+
                                 Console.WriteLine($"Odebrano wiadomość: {message.Date} {uid}");  
-                            }          
+                                // Wykonanie zapytania PUT
+                                await SendPutRequest(jsonData);
+                                // Wykonanie zapytania GET
+                                await SendGETRequest();
+                            }
+                            
+                            
+                            if (message.Subject.Contains("START", StringComparison.OrdinalIgnoreCase))
+                            {
+                                string jsonData = "{ \"status\": \"enable\" }";
+
+                                Console.WriteLine($"Odebrano wiadomość: {message.Date} {uid}");  
+                                // Wykonanie zapytania PUT
+                                await SendPutRequest(jsonData);
+                                // Wykonanie zapytania GET
+                                await SendGETRequest();
+                            } 
+
+
                             // Po przetworzeniu wiadomości możesz ją oznaczyć jako przeczytaną
                          await inbox.AddFlagsAsync(uid, MessageFlags.Seen, true);
                          previousMessageCount = inbox.Count;
@@ -88,6 +94,7 @@ class Program
                     // Sprawdzenie co 10 sekund
                     await Task.Delay(10000, cancellationTokenSource.Token);
                     }
+                    
                 }
 
                  // Rozłącz się z serwerem IMAP
@@ -101,9 +108,48 @@ class Program
             {
                 Console.WriteLine($"Wystąpił błąd: {ex.Message}");
             }
-            //await Task.Delay(5000); // Symulacja pracy trwającej 20 sekund
-        }
+           
+        }    
     }
+
+    private static async Task SendPutRequest(string jsonData)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+                var response = await httpClient.PutAsync("http://10.10.10.4/api/v2/cmdb/firewall/policy/3/?access_token=gtjmQkf4s1kQwQ9yd3nhmfxx39ykb9", content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("Dane zostały pomyślnie zaktualizowane na serwerze.");
+                }
+                else
+                {
+                    Console.WriteLine($"Wystąpił błąd: {response.StatusCode}");
+                }
+            }
+        }
+    
+    private static async Task SendGETRequest()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                
+                HttpResponseMessage response = await httpClient.GetAsync("http://10.10.10.4/api/v2/cmdb/firewall/policy/3?access_token=gtjmQkf4s1kQwQ9yd3nhmfxx39ykb9");
+                string json = await response.Content.ReadAsStringAsync();
+                JObject jsonObject = JObject.Parse(json);
+                     foreach (var result in jsonObject["results"])
+                        {
+                            if ((int)result["policyid"] == 3)
+                            {
+                                // Wyświetlanie statusu
+                                Console.WriteLine($"Nazwa policy: {result["name"]}");
+                                Console.WriteLine($"Status dla policy z id 3: {result["status"]}");
+                                Console.WriteLine($"Data zmiany statusu: {DateTime.Now}");
+                            }
+                        }
+            }
+        }
 }
 
 
